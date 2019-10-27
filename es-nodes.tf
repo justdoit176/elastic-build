f8a56edbea6f727ced723a49d07abbd9ad378cf9
Fabio Rosati <>
2019-10-28 00:06:20 +0100

    Prepare for multiple instances creation

--- es-nodes.tf
+++ es-nodes.tf
@@ -1,8 +1,14 @@
-module "ubuntu_node" {
+module "ubuntu_node_1" {
   source = "./ubuntu_node"
   instance_name = "my_es_1"
   base_img_url = "/var/lib/libvirt/images/BASE/bionic-server-cloudimg-amd64.img"
 }
+
+#module "ubuntu_node_2" {
+#  source = "./ubuntu_node"
+#  instance_name = "my_es_2"
+#  base_img_url = "/var/lib/libvirt/images/BASE/bionic-server-cloudimg-amd64.img"
+#}
 
 terraform {
   required_version = ">= 0.12"
