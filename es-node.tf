5d54a8f91a42c43a6fef0bf868188a9f236c2863
Fabio Rosati <>
2019-10-27 14:38:04 +0100

    Ubuntu instantiation works

---
+++ es-node.tf
@@ -0,0 +1,82 @@
+# instance the provider
+provider "libvirt" {
+  uri = "qemu:///system"
+}
+
+resource "libvirt_pool" "elastic" {
+  name = "elastic"
+  type = "dir"
+  path = "/var/lib/libvirt/images/elastic"
+}
+
+# We fetch the latest ubuntu release image from their mirrors
+resource "libvirt_volume" "elastic-qcow2" {
+  name   = "elastic-qcow2"
+  pool   = libvirt_pool.elastic.name
+  source = "https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img"
+#  source = "/var/lib/libvirt/images/BASE/ubuntu-18.04-minimal-cloudimg-amd64.qcow2"
+  format = "qcow2"
+}
+
+data "template_file" "user_data" {
+  template = file("${path.module}/cloud_init.cfg")
+}
+
+data "template_file" "network_config" {
+  template = file("${path.module}/network_config.cfg")
+}
+
+# for more info about paramater check this out
+# https://github.com/dmacvicar/terraform-provider-libvirt/blob/master/website/docs/r/cloudinit.html.markdown
+# Use CloudInit to add our ssh-key to the instance
+# you can add also meta_data field
+resource "libvirt_cloudinit_disk" "commoninit" {
+  name           = "commoninit.iso"
+  user_data      = data.template_file.user_data.rendered
+  network_config = data.template_file.network_config.rendered
+  pool           = libvirt_pool.elastic.name
+}
+
+# Create the machine
+resource "libvirt_domain" "domain-es-n1" {
+  name   = "es-n1"
+  memory = "2048"
+  vcpu   = 2
+
+  cloudinit = libvirt_cloudinit_disk.commoninit.id
+
+  network_interface {
+    network_name = "default"
+  }
+
+  # IMPORTANT: this is a known bug on cloud images, since they expect a console
+  # we need to pass it
+  # https://bugs.launchpad.net/cloud-images/+bug/1573095
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
+
+  disk {
+    volume_id = libvirt_volume.elastic-qcow2.id
+  }
+
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
+}
+
+terraform {
+  required_version = ">= 0.12"
+}
+
+# IPs: use wait_for_lease true or after creation use terraform refresh and terraform show for the ips of domain
--- one.tf
+++
@@ -1,7 +0,0 @@
-provider "libvirt" {
-    uri = "qemu:///system"
-}
-
-resource "libvirt_domain" "es-n2" {
-  name = "es-n2"
-}
