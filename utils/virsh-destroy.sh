2a0bf579fcd95fe04dd56e169c83c73ad62ed590
Fabio Rosati <>
2020-03-28 17:51:18 +0100

    Removes unused files

--- utils/virsh-destroy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-NAME=$1
-
-virsh --connect qemu:///system destroy --domain=${NAME}
-virsh --connect qemu:///system undefine --domain=${NAME}
-virsh --connect qemu:///system vol-delete ${NAME}.qcow2 --pool default
